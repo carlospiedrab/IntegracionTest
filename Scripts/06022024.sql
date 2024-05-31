5d775da77094458641553255a7fdeffa54b770d6
carlospiedrab <30283007+carlospiedrab@users.noreply.github.com>
2024-05-31 00:17:26 -0700

    Update 06022024.sql

--- Scripts/06022024.sql
+++ Scripts/06022024.sql
@@ -1,1 +1,50 @@
 
+
+
+ Create table Categoria (
+   CategoriaId int not null IDENTITY,
+   Nombre  nvarchar(120) not null
+  );
+  
+  Alter table Categoria
+  Add Constraint Pk_Categoria PRIMARY KEY (CategoriaId);
+
+ Create table Marca (
+   MarcaId int not null IDENTITY,
+   Nombre  nvarchar(120) not null
+  );
+  
+  Alter table Marca
+  Add Constraint Pk_Marca PRIMARY KEY (MarcaId);
+
+  Create table Producto (
+    ProductoId int not null IDENTITY,
+	NombreProducto nvarchar(120) not null,
+	Precio numeric(7,2) not null,
+	Costo numeric(7,2) not null,
+    CategoriaId int not null,
+	MarcaId int not null
+  );
+  
+  Alter table Producto
+  Add constraint Pk_Producto PRIMARY KEY (ProductoId);
+  
+  ALTER Table Producto
+  Add Constraint Fk_Producto_Categoria_CategoriaId FOREIGN KEY(CategoriaId)
+  REFERENCES Categoria (CategoriaId) ON DELETE CASCADE;
+
+  ALTER Table Producto
+  Add Constraint Fk_Producto_Marca_MarcaId FOREIGN KEY(MarcaId)
+  REFERENCES Marca (MarcaId) ON DELETE CASCADE;
+
+   Insert into Categoria (nombre)
+   values ('Computadoras');
+
+   Insert into Categoria (nombre)
+   values ('Impresoras');
+
+   Insert into Marca (nombre)
+   values ('HP');
+
+   Insert into Marca (nombre)
+   values ('Apple');
