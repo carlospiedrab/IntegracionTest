5e6d7ea2bb0ab2d4c2a338187aa016e9e7aa0a73
Carlos Piedra <barce90@hotmail.com>
2024-06-07 01:25:43 -0700

    Agregar endpoints para Producto Controller

--- Data/ProductoData.cs
+++ Data/ProductoData.cs
@@ -10,10 +10,10 @@ namespace Data
     {
         public static List<Producto> listaProducto = new List<Producto>
         {
-            new Producto { Id = 1, NombreProducto="Producto 1", Categoria="Computadoras",Marca="HP", Precio=2000, Costo=1000},
-            new Producto { Id = 2, NombreProducto="Producto 2", Categoria="Computadoras",Marca="Windows", Precio=2600, Costo=1100},
-            new Producto { Id = 3, NombreProducto="Producto 3", Categoria="Computadoras",Marca="HP", Precio=2300, Costo=1200},
-            new Producto { Id = 4, NombreProducto="Producto 4", Categoria="Computadoras",Marca="HP", Precio=3000, Costo=1300},
+            new Producto { Id = 1, NombreProducto="Producto 1", CategoriaId=1,MarcaId=1, Precio=2000, Costo=1000},
+            new Producto { Id = 2, NombreProducto="Producto 2", CategoriaId=1,MarcaId=1, Precio=2600, Costo=1100},
+            new Producto { Id = 3, NombreProducto="Producto 3", CategoriaId=1,MarcaId=1, Precio=2300, Costo=1200},
+            new Producto { Id = 4, NombreProducto="Producto 4", CategoriaId=1,MarcaId=1, Precio=3000, Costo=1300},
         };
     }
 }
